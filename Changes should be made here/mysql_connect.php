d203c52d8278ce78c3b92d7cb2b6aff47799cb19
Martin Aaby Finnerup <hackerfinn@gmail.com>
2012-12-04 09:18:11 +0100

    Moved working directory.
    Moved the working directory for adding to develeopment branch.

--- Changes should be made here/mysql_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$mysqlhost = "HOST";
-    $mysqluser = "USERNAME";
-    $mysqlpw = "MYSQLPW";
-	$connection=mysql_connect("$mysqlhost", "$mysqluser", "$mysqlpw") or die
-    ("Failed to connect.");
-?>
